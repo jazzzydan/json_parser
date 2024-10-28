353efa571f9fd130a01a782d11425afdfcf4aa52
Daniil Starov <jazzzydan@hotmail.com>
2024-10-28 22:35:37 +0200

    trim(), split() and jsonToMap() methods implemented.

--- src/main/java/org/example/JsonParser.java
+++ src/main/java/org/example/JsonParser.java
@@ -94,19 +94,4 @@ public class JsonParser {
 
 
     }
-
-    public static void main(String[] args) {
-        String jsonString = "{\n" +
-                "  \"key 1\": \"value with spaces\",\n" +
-                "  \"key 2\": true,\n" +
-                "  \"key 3\": null,\n" +
-                "  \"key 4\": 42,\n" +
-                "  \"key 5\": {\n" +
-                "    \"nested Key 1\": \"nestedValue\"\n" +
-                "  }\n" +
-                "}";
-
-        JsonParser parser = new JsonParser();
-        parser.parse(jsonString);
-    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        String jsonString = "{\n" +
+                "  \"key 1\": \"value with spaces\",\n" +
+                "  \"key 2\": true,\n" +
+                "  \"key 3\": null,\n" +
+                "  \"key 4\": 42,\n" +
+                "  \"key 5\": {\n" +
+                "    \"nested Key 1\": \"nestedValue\"\n" +
+                "  }\n" +
+                "}";
 
+        JsonParser parser = new JsonParser();
+        parser.parse(jsonString);
 
     }
 }
