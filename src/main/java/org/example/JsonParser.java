68c0650db5f6ccc9db4fd62f5ee03b788aea40f1
Daniil Starov <jazzzydan@hotmail.com>
2024-10-28 22:32:22 +0200

    trim(), split() and jsonToMap() methods implemented.

--- src/main/java/org/example/JsonParser.java
+++ src/main/java/org/example/JsonParser.java
@@ -97,9 +97,13 @@ public class JsonParser {
 
     public static void main(String[] args) {
         String jsonString = "{\n" +
-                "  \"key 1\": \"value with spaces\", \n" +
+                "  \"key 1\": \"value with spaces\",\n" +
                 "  \"key 2\": true,\n" +
-                "  \"key 3\": null\n" +
+                "  \"key 3\": null,\n" +
+                "  \"key 4\": 42,\n" +
+                "  \"key 5\": {\n" +
+                "    \"nested Key 1\": \"nestedValue\"\n" +
+                "  }\n" +
                 "}";
 
         JsonParser parser = new JsonParser();
