8b650d1ba843165c83a48cd0af0278e86a10ce4e
Daniil Starov, Liis Schotter <jazzzydan@hotmail.com, liisschotter@gmail.com>
2024-10-29 11:56:08 +0200

    parse() method updated

--- src/main/java/org/example/JsonParser.java
+++ src/main/java/org/example/JsonParser.java
@@ -1,80 +1,113 @@
 package org.example;
 
-import java.util.ArrayList;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonParser {
 
-    private String json;
-    private int index;
+    public Object parse(String input) {
+        if (input == null) return null;
+        if (input.matches("-?[0-9]+")) return Integer.valueOf(input);
+        if (input.matches("-?\\d+(\\.\\d+)?")) return Double.valueOf(input);
+        if (input.matches("true|false")) return Boolean.valueOf(input);
 
-    public JsonParser(String json) {
-        this.json = json;
-        this.index = 0;
+
+
+        return null;
     }
 
-    public boolean validate() {
-        json.trim();
-        if (json.charAt(index) == '{' && json.charAt(json.length() - 1) == '}') {
-            return true;
-        }
-        return false;
+    public Object parse(Reader input) {
+
+        return null;
     }
 
-    public String trim() {
-        String singleLineJson = json.replaceAll("\\s+", " ").trim();
-        StringBuilder trimmed = new StringBuilder();
-        boolean inQuotes = false;
 
-        for (int index = 0; index < singleLineJson.length(); index++) {
-            char currentChar = singleLineJson.charAt(index);
-
-            if (currentChar == '"') {
-                inQuotes = !inQuotes;
-                trimmed.append(currentChar);
-            } else if (currentChar != ' ') {
-                trimmed.append(currentChar);
-            } else if (inQuotes) {
-                trimmed.append(currentChar);
-            }
-        }
-        return trimmed.toString();
-    }
-
-    public void parse() {
-        ArrayList<Object> objects = new ArrayList<>();
-        objects.add("Test");
-        objects.add("Test2");
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("Test", true);
-        map.put("Test1", 54);
-        map.put("Test2", null);
-        map.put("Test3", "tere");
-        map.put("objectList", objects);
-
-        Map<String, Object> rebane = new HashMap<>();
-        rebane.put("name", "Rein");
-
-
-
-//        System.out.println(objects);
-//        System.out.println(map);
-        System.out.println(rebane);
-
-
-    }
-
-    public static void main(String[] args) {
-        String jsonString = "{\n" +
-                "  \"key 1\": \"value with spaces\", \n" +
-                "  \"key 2\": true,\n" +
-                "  \"key 3\": null\n" +
-                "}";
-
-        JsonParser parser = new JsonParser(jsonString);
-        String trimmedJson = parser.trim();
-        System.out.println(trimmedJson);
-    }
 }
+//    public Object parse(Reader input) {
+//
+//        return null;
+//    }
+//
+//    public Object parse(String input) {
+//        return new StringReader(input);
+
+//        if (!isValidJson(json)) {
+//            throw new IllegalArgumentException("Invalid JSON format");
+//        }
+//
+//        String trimmedJson = trim(json);
+//        String[] keyValuePairs = split(trimmedJson);
+//        Map<String, Object> jsonMap = jsonToMap(keyValuePairs);
+//
+//        System.out.println(jsonMap.keySet());
+//
+//
+//    private String parseString(String input) {
+//        StringBuilder output = new StringBuilder();
+//        for (int index = 0; index < input.length(); index++) {
+//            char currentChar = input.charAt(index);
+//            if (currentChar != '"') { output.append(currentChar); }
+//        }
+//        return output.toString();
+//    }
+//
+//    boolean isValidJson(String json) {
+//        if (hasProperWrapper(json)) {
+//            return true;
+//        }
+//        return false;
+
+//    }
+//
+//    boolean hasProperWrapper(String json) {
+//        int index = 0;
+//        return json.charAt(index) == '{' && json.charAt(json.length() - 1) == '}';
+//    }
+//
+//    String trim(String json) {
+//        String singleLineJson = stringifyJson(json);
+//        singleLineJson = removeWrapper(singleLineJson);
+//        StringBuilder trimmed = new StringBuilder();
+//        boolean inQuotes = false;
+//
+//        for (int index = 0; index < singleLineJson.length(); index++) {
+//            char currentChar = singleLineJson.charAt(index);
+//
+//            if (currentChar == '"') {
+//                inQuotes = !inQuotes;
+//                trimmed.append(currentChar);
+//            } else if (currentChar != ' ') {
+//                trimmed.append(currentChar);
+//            } else if (inQuotes) {
+//                trimmed.append(currentChar);
+//            }
+//        }
+//        return trimmed.toString();
+//    }
+//
+//    String stringifyJson(String json) {
+//        return json.replaceAll("\\s+", " ").trim();
+//    }
+//
+//    String removeWrapper(String singleLineJson) {
+//        return singleLineJson.substring(1, singleLineJson.length() - 1).trim();
+//    }
+//
+//    String[] split(String trimmedJson) {
+//        return trimmedJson.split(",");
+//    }
+//
+//    Map<String, Object> jsonToMap(String[] keyValuePairs) {
+//        Map<String, Object> jsonMap = new HashMap<>();
+//        for (String keyValuePair : keyValuePairs) {
+//            String[] keyValue = keyValuePair.split(":");
+//            String key = keyValue[0].trim();
+//            Object value = keyValue[1].trim();
+//            jsonMap.put(key, value);
+//        }
+//
+//        return jsonMap;
+//    }
+
--- src/test/java/org/example/JsonParserTest.java
+++ src/test/java/org/example/JsonParserTest.java
@@ -1,45 +1,96 @@
 package org.example;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JsonParserTest {
 
+    JsonParser parser = new JsonParser();
+
     @Test
-    void validateTest() {
-        JsonParser parser = new JsonParser("{\n\"key\": \"string\"\n}");
-        assertTrue(parser.validate());
-        parser = new JsonParser("{\n\"key\": \"string\"\n");
-        assertFalse(parser.validate());
-
-        parser.parse();
-
+    void parseStringTest() {
+        assertEquals("value", parser.parse("\"value\""));
     }
 
     @Test
-    void trimTest() {
-        JsonParser parser = new JsonParser("{\n" +
-                "  \"key 1\": \"value with spaces\", \n" +
-                "  \"key 2\": true,\n" +
-                "  \"key 3\": null\n" +
-                "}");
-
-        assertEquals("{\"key 1\":\"value with spaces\",\"key 2\":true,\"key 3\":null}", parser.trim());
+    void parseNullTest() {
+        assertNull(parser.parse("null"));
     }
 
     @Test
-    void splitTest() {
-        JsonParser parser = new JsonParser("{\"key 1\":\"value with spaces\",\"key 2\":true,\"key 3\":null}");
-        assertEquals(ArrayList<>);
+    void parseIntegerTest() {
+        assertEquals(-101, parser.parse("-101"));
     }
+
+    @Test
+    void parseDoubleTest() {
+        assertEquals(-333.1457, parser.parse("-333.1457"));
+    }
+
+    @Test
+    void parseTrueBooleanTest() {
+        assertEquals(true, parser.parse("true"));
+    }
+
+    @Test
+    void parseFalseBooleanTest() {
+        assertEquals(false, parser.parse("false"));
+    }
+
+
+
 
 //    @Test
+//    void validateTest() {
+//        assertTrue(parser.isValidJson("{\n\"key\": \"string\"\n}"));
+//        assertFalse(parser.isValidJson("{\n\"key\": \"string\"\n"));
+//    }
+//
+//    @Test
+//    void trimTest() {
+//        // language=json
+//        String json = """
+//                {
+//                  "key 1": "value with spaces",
+//                  "key 2": true,
+//                  "key 3": null
+//                }""";
+//        assertEquals("\"key 1\":\"value with spaces\",\"key 2\":true,\"key 3\":null", parser.trim(json));
+//    }
+//
+//    @Test
+//    void splitTest() {
+//        String json = "\"key 1\":\"value with spaces\",\"key 2\":true,\"key 3\":null";
+//        String[] expected = {"\"key 1\":\"value with spaces\"", "\"key 2\":true", "\"key 3\":null"};
+//        assertArrayEquals(expected, parser.split(json));
+
+//    }
+//    @Test
+//    void jsonToMapTest() {
+//        String[] keyValuePairs = {
+//                "\"key 1\":\"value with spaces\"",
+//                "\"key 2\":true",
+//                "\"key 3\":null"
+//        };
+//        Map<String, Object> expectedMap = Map.of(
+//                "\"key 1\"", "value with spaces",
+//                "\"key 2\"", "true",
+//                "\"key 3\"", "null"
+//        );
+//
+//        Map<String, Object> result = parser.jsonToMap(keyValuePairs);
+//        assertEquals(expectedMap, result);
+
+//    }
+//    @Test
 //    void stringValueTest() {
-//        JsonParser jsonParser = new JsonParser("{\n\"key\": \"string\"\n}");
-//        assertEquals("string", jsonObject.getValue());
+//        String[] keyValuePairs = {"\"key\":\"string\""};
+//        assertEquals("string", parser.jsonToMap(keyValuePairs).get("\"key\""));
+
 //    }
 
 }
